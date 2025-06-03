1547f9a0c9e4b9ce206d778fc979f4543f105fc2
Constantin <Constantin_CK@outlook.com>
2025-06-03 15:42:29 +0300

    feat: Add task 1 solution

---
+++ task1/solution.py
@@ -0,0 +1,35 @@
+def strict(func):
+    def wrapper(*args, **kwargs):
+        annotations = func.__annotations__
+        exception_msg = 'Аргумент {} должен быть {} а не {}'
+
+        for n, i in enumerate(args):
+            if list(annotations.values())[n] and not isinstance(i, list(annotations.values())[n]):
+                raise TypeError(
+                    exception_msg.format(
+                        list(annotations.keys())[n],
+                        list(annotations.values())[n].__name__,
+                        type(i).__name__
+                    )
+                )
+
+        for k, v in kwargs.items():
+            if annotations.get(k) and not isinstance(v, annotations.get(k)):
+                raise TypeError(
+                    exception_msg.format(
+                        k,
+                        annotations.get(k).__name__,
+                        type(v).__name__
+                    )
+                )
+        return func(*args, **kwargs)
+
+    return wrapper
+
+
+@strict
+def some_fn(a: int, b: int) -> str:
+    return f'b -- {a}'
+
+
+print(some_fn(a=1, b=2.5))
